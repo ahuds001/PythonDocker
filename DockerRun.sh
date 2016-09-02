24c7aba4d4c2401bce34faef123f498aca5400ea
Tony <ahuds001@gmail.com>
2016-09-02 11:18:28 -0500

    DockerRun fix 2

--- DockerRun.sh
+++ DockerRun.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker run -v ~/PythonDocker/TalkingData:/home/TalkingData -v ~/PythonDocker/Notebooks:/home/Notebooks -it -p 8888:8888 --name kaggle01 ahuds001/python:0.2 /bin/bash -c "export jupyter notebook --debug --notebook-dir=/home/Notebooks --ip='*' --port=8888 --no-browser"
+docker run -v ~/PythonDocker/TalkingData:/home/TalkingData -v ~/PythonDocker/Notebooks:/home/Notebooks -it -p 8888:8888 --name kaggle01 ahuds001/python:0.2 /bin/bash -c "jupyter notebook --debug --notebook-dir=/home/Notebooks --ip='*' --port=8888 --no-browser"
